0f39148f6262d3e4dc02aef0e88cdb1c9e9d157b
Vithor112 <vithorpileco@gmail.com>
2025-11-10 00:23:44 -0300

    feat: update StratifiedKFold to be Repeated StratifiedKFold with n=10

--- model.py
+++ model.py
@@ -115,18 +115,23 @@ def calculate_pulse_pressure(df):
     df.insert(insert_loc, 'pulse_pressure', pp_values)
     return df
 
+
+REPETITION_COUNT = 10
+
 df = load_health_data()
 df = encode_categorical_columns(df)
 df = calculate_pulse_pressure(df)
 df = drop_unnecessary_columns(df)
 X, Y = separate_features_and_target(df)
-skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-for train, test in skf.split(X, Y):
-    X_train, X_test = X[train], X[test]
-    Y_train, Y_test = Y[train], Y[test]
-    X_train, X_test = standardize_numeric_columns(pd.DataFrame(X_train, columns=df.columns[:-1]), pd.DataFrame(X_test, columns=df.columns[:-1]))
-    X_train, Y_train = smote_oversampling(X_train.values, Y_train)
-    # TODO Realizar treinamento e avaliação do modelo aqui (Random forest, Nayve Bayes, Decision Tree com poda, Rede Neural, Regressão logistica, Knn (3,5,7 vizinhos))
-    # TODO calcular métricas de avalição do modelo (AUC Sensitivity Specificity Geometric_Mean Precision)
-    # TODO necessário salvar métricas em um arquivo .csv para análise posterior
-    # necessitamos ter varias metricas para cada modelo para calcular desvio padrão e média e significancia estatistica
+for repetition in range(REPETITION_COUNT):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    for train, test in skf.split(X, Y):
+        X_train, X_test = X[train], X[test]
+        Y_train, Y_test = Y[train], Y[test]
+        X_train, X_test = standardize_numeric_columns(pd.DataFrame(X_train, columns=df.columns[:-1]), pd.DataFrame(X_test, columns=df.columns[:-1]))
+        X_train, Y_train = smote_oversampling(X_train.values, Y_train)
+        # TODO Realizar treinamento e avaliação do modelo aqui (Random forest, Nayve Bayes, Decision Tree com poda, Rede Neural, Regressão logistica, Knn (3,5,7 vizinhos))
+        # TODO calcular métricas de avalição do modelo (AUC Sensitivity Specificity Geometric_Mean Precision)
+        # TODO necessário salvar métricas em um arquivo .csv para análise posterior
+        # necessitamos ter varias metricas para cada modelo para calcular desvio padrão e média e significancia estatistica 
+        # TODO modelo header csv (Model, Repetition, Fold, AUC, Sensitivity, Specificity, Geometric_Mean, Precision)
